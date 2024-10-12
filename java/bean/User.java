a50fa966fcf93ec37053f3206aa1f0d87251d07a
nguyenphi0604 <phinguyen06042004@gmail.com>
2024-10-12 09:15:06 +0700

    Create User.java

---
+++ java/bean/User.java
@@ -0,0 +1,28 @@
+package bean;
+
+public class User {
+	String usernameOrEmail;
+	String password;
+
+	public User(String usernameOrEmail, String password) {
+		this.usernameOrEmail = usernameOrEmail;
+		this.password = password;
+	}
+
+	public String getUsernameOrEmail() {
+		return usernameOrEmail;
+	}
+
+	public void setUsernameOrEmail(String usernameOrEmail) {
+		this.usernameOrEmail = usernameOrEmail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
